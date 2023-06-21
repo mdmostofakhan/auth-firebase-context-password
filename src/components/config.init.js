54dbd8ae5b5cc7b5ab6e6764d2ddddad9158f484
mostofa khan <mdmostofakhan1972@gmail.com>
2023-06-21 22:00:59 +0600

    tailwind the join password

---
+++ src/components/config.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAeEBQ3KEMSMPeBSPOTiWEZCJJBjb3yyoA",
+  authDomain: "auth-firebase-context-password.firebaseapp.com",
+  projectId: "auth-firebase-context-password",
+  storageBucket: "auth-firebase-context-password.appspot.com",
+  messagingSenderId: "384917760430",
+  appId: "1:384917760430:web:e88460fe4db44b400a803f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app ;
---
+++ src/components/layout/Register.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Register = () => {
+    return (
+        <div>
+            <h3>Please me Register</h3>
+        </div>
+    );
+};
+
+export default Register;
